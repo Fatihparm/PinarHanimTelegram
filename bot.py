4711ac5244071358f126f902aa374da0cc9544bc
Fatih Parmaksız <fatihparm6@gmail.com>
2025-01-03 19:04:00 +0300

    subscription added

--- bot.py
+++ bot.py
@@ -1,51 +1,45 @@
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackContext
 from dotenv import load_dotenv
-from web_scraping import Scrape
-from data_models import Model
 import random
 import os
 import logging
 from datetime import time, datetime
 
-# Load environment variables
+from web_scraping import Scrape
+from data_models import Model
+
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 logging.basicConfig(
-    format="%(asctime)s [%(levelname)s] %(message)s", 
-    datefmt="%Y-%m-%d %H:%M:%S", 
-    level=logging.INFO  
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
 )
-
 logging.getLogger("httpx").setLevel(logging.WARNING)
-logger = logging.getLogger("TelegramBot") 
+logger = logging.getLogger("TelegramBot")
 
 model = Model()
 scrape = Scrape()
 model.create_tables()
 
 horoscope_signs = [
-    "balik", "kova", "oglak", "yay", "akrep", "terazi",
-    "basak", "aslan", "yengec", "ikizler", "boga", "koc"
-] # Turkish names of zodiac signs
+        "balik", "kova", "oglak", "yay", "akrep", "terazi",
+        "basak", "aslan", "yengec", "ikizler", "boga", "koc"
+    ]
 
-def restart(context: CallbackContext):    
+def restart(context: CallbackContext):
     logger.info("Bot is restarting...")
     try:
-        try:
-            model.clear_zodiac_signs()
-            model.clear_tarot_cards()
-            logging.info("Database is cleared.")
-        except Exception as e:
-            logger.error(f"An error occurred while clearing the database: {e}")
-        try:
-            scrape.sign_content_push()  # Scrape horoscope content and push to database
-            scrape.tarot_content_push()  # Scrape tarot content and push to database
-            model.remove_duplicates_if_exists()
-            logger.info("Database is updated.")
-        except Exception as e:
-            logger.error(f"An error occurred while updating the database: {e}")
+        model.clear_zodiac_signs()
+        model.clear_tarot_cards()
+        logger.info("Database is cleared.")
+        
+        scrape.sign_content_push()  # Scrape horoscope content and push to database
+        scrape.tarot_content_push()  # Scrape tarot content and push to database
+        model.remove_duplicates_if_exists()
+        logger.info("Database is updated.")
     except Exception as e:
         logger.error(f"An error occurred while restarting the bot: {e}")
 
@@ -54,33 +48,94 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is ready. Use /tarot or /burc <sign> commands.")
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = """Günlük burç yorumları ve tarot falı hakkında bilgi almak için aşağıdaki komutları kullanabilirsiniz:\n\n
-    /tarot: Rastgele bir tarot kartı çeker.\n\n/burc <burç>: Belirtilen burcun günlük yorumunu gösterir. (Örnek: /burc kova)
-    """
-    await update.message.reply_text()
+    text = (
+        "Günlük burç yorumları ve tarot falı hakkında bilgi almak için aşağıdaki komutları kullanabilirsiniz:\n\n"
+        "/tarot: Rastgele bir tarot kartı çeker.\n\n"
+        "/burc <burç>: Belirtilen burcun günlük yorumunu gösterir. (Örnek: /burc kova)"
+    )
+    await update.message.reply_text(text)
+
+async def abonelik(update: Update, context: ContextTypes.DEFAULT_TYPE):    
+    user = update.message.from_user
+    first_name = user["first_name"]
+    last_name = user["last_name"]
+    telegram_id = user["id"]
+    check_id = model.check_person(telegram_id)
+    zodiac_sign = None
+    
+    try:
+        if check_id is None:
+            # Check if arguments are provided
+            if not context.args:
+                await update.message.reply_text(
+                    "Lütfen burcunuzu girin. (Örnek: /abonelik kova)\n\n Burçlar:\n balik, kova, oglak, yay, akrep, terazi, basak, aslan, yengec, ikizler, boga, koc"
+                )
+                return
+            
+            zodiac_sign = context.args[0].lower()
+            if zodiac_sign not in horoscope_signs:
+                await update.message.reply_text(
+                    "Geçerli bir burç girin. (Örnek: /abonelik kova)\n\n Burçlar:\n balik, kova, oglak, yay, akrep, terazi, basak, aslan, yengec, ikizler, boga, koc"
+                )
+                return
+            
+            model.add_user(telegram_id, first_name, last_name, zodiac_sign)
+            await update.message.reply_text(
+                f"Abonelik kaydınız oluşturuldu! Günlük burç yorumlarınız {zodiac_sign} burcu için paylaşılacaktır."
+            )
+            return
+        else:
+            await update.message.reply_text("Zaten bir aboneliğiniz var.")
+            return
+    except Exception as e:
+        logger.error(f"An error occurred while checking the subscription: {e}")
+        await update.message.reply_text("Bir hata oluştu, lütfen daha sonra tekrar deneyin.")
+        return
+
+async def abonelikiptal(update:Update, context:ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    telegramId= user["id"]
+    check_id = model.check_person(telegramId)
+    if check_id is None:
+        text = "Aboneliğiniz bulunmamaktadır."
+        await update.message.reply_text(text)
+    else:
+        model.delete_person(telegramId)
+        text = "Aboneliğiniz iptal edilmiştir."
+        await update.message.reply_text(text)
+    info = update.message
+    messages_to_add(info)
+    logger.info(f"/abonelikiptal komutu tamamlandı. Kullanıcı: {user.first_name} ({user.id})")
+
 
 async def tarot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    info = update.message
     try:
         tarot_cards = model.get_tarot_cards()
         random_card = random.choice(tarot_cards)
         embed = f"{random_card.name}\n\n{random_card.description}"
+        
         if random_card.image:
             await context.bot.send_photo(
                 chat_id=update.effective_chat.id,
-                photo=random_card.image,         
-                caption=embed                 
+                photo=random_card.image,
+                caption=embed
             )
+            messages_to_add(info)
         else:
             await update.message.reply_text(embed)
-
+            messages_to_add(info)
     except Exception as e:
         logger.error(f"An error occurred while getting a tarot card: {e}")
         await update.message.reply_text("Bir hata oluştu, lütfen daha sonra tekrar deneyin.")
 
-    
 async def burc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    info = update.message
     if len(context.args) == 0:
-        await update.message.reply_text("Lütfen bir burç ismi girin. (Türkçe karakter kullanmayınız.)\n\n burçlar: balik, kova, oglak, yay, akrep, terazi, basak, aslan, yengec, ikizler, boga, koc")
+        await update.message.reply_text(
+            "Lütfen bir burç ismi girin. (Türkçe karakter kullanmayınız.)\n\n"
+            "Burçlar: balik, kova, oglak, yay, akrep, terazi, basak, aslan, yengec, ikizler, boga, koc"
+        )
         return
 
     arg = context.args[0].lower()
@@ -92,18 +147,31 @@ async def burc(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 if sign.image:
                     await context.bot.send_photo(
                         chat_id=update.effective_chat.id,
-                        photo=sign.image,         
-                        caption=embed                 
+                        photo=sign.image,
+                        caption=embed
                     )
+                    messages_to_add(info)
                 else:
                     await update.message.reply_text(embed)
+                    messages_to_add(info)
                 break
     else:
-        await update.message.reply_text("Geçerli bir burç ismi giriniz. (Türkçe karakter kullanmayınız.)\n\n burçlar: balik, kova, oglak, yay, akrep, terazi, basak, aslan, yengec, ikizler, boga, koc")
+        await update.message.reply_text(
+            "Geçerli bir burç ismi giriniz. (Türkçe karakter kullanmayınız.)\n\n"
+            "Burçlar: balik, kova, oglak, yay, akrep, terazi, basak, aslan, yengec, ikizler, boga, koc"
+        )
 
-def callbackRestart(context: ContextTypes.DEFAULT_TYPE):
-    timer = time(hour=17, minute=0, second=0)
-    context.job_queue.run_daily(restart, timer, days=(0,1,2,3,4,5,6))
+def messages_to_add(info):
+    user = info.from_user
+    first_name = user["first_name"]
+    last_name = user["last_name"]
+    telegramId = user["id"]
+    message = info.text
+    model.add_message(telegramId, first_name, last_name, message)
+
+def callback_restart(context: ContextTypes.DEFAULT_TYPE):
+    timer = time(hour=20, minute=59, second=0)
+    context.job_queue.run_daily(restart, timer, days=(0, 1, 2, 3, 4, 5, 6))
 
 # Main function to start the bot
 def main():
@@ -111,11 +179,12 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("tarot", tarot))
     application.add_handler(CommandHandler("burc", burc))
+    application.add_handler(CommandHandler("abonelik", abonelik))
+    application.add_handler(CommandHandler("abonelikiptal", abonelikiptal))
     restart(application)
-    callbackRestart(application, )
+    callback_restart(application)
     
     application.run_polling()
 
 if __name__ == "__main__":
     main()
-
--- data_models.py
+++ data_models.py
@@ -2,28 +2,97 @@ import sqlite3
 
 class Model:
     def __init__(self):
-        self.conn = sqlite3.connect('database.db')
-        self.c = self.conn.cursor()
+        self.db = sqlite3.connect('database.db', check_same_thread=False)
+        self.c = self.db.cursor()
         self.create_tables()
 
     def create_tables(self):
-        self.c.execute('''CREATE TABLE IF NOT EXISTS ZodiacSigns
-                          (id INTEGER PRIMARY KEY, name TEXT, description TEXT, date TEXT, image TEXT)''')
-        self.c.execute('''CREATE TABLE IF NOT EXISTS TarotCards
-                          (id INTEGER PRIMARY KEY, name TEXT, description TEXT, image TEXT)''')
-        self.conn.commit()
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS ZodiacSigns(
+                id INTEGER PRIMARY KEY,
+                name TEXT DEFAULT "", 
+                description TEXT DEFAULT "",
+                date TEXT,
+                image TEXT
+            )''')
+            
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS TarotCards(
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL, 
+                description TEXT DEFAULT "",
+                image TEXT DEFAULT ""
+            )''')
+        
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY,
+                chat_id TEXT NOT NULL, 
+                first_name TEXT DEFAULT "",
+                last_name TEXT DEFAULT "",
+                message TEXT DEFAULT "",
+                message_date DATETIME DEFAULT CURRENT_TIMESTAMP
+            )''')
+        
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS subscriptions (
+                chat_id TEXT PRIMARY KEY NOT NULL, 
+                first_name TEXT DEFAULT "",
+                last_name TEXT DEFAULT "",
+                sign TEXT DEFAULT "",
+                subsdate DATETIME DEFAULT CURRENT_TIMESTAMP
+            )    
+            ''')
+        self.db.commit()
+    
+    def add_message(self, chat_id, first_name, last_name, message):
+        """Adds a message to the database"""
+        self.c.execute(
+        '''
+        INSERT INTO messages (chat_id, first_name, last_name, message) 
+        VALUES (?, ?, ?, ?)
+        ''', (chat_id, first_name, last_name, message))
+        self.db.commit()
 
     def insert_zodiac_sign(self, sign):
         self.c.execute("INSERT INTO ZodiacSigns (name, description, date, image) VALUES (?, ?, ?, ?)", (sign.name, sign.description, sign.date, sign.image))
-        self.conn.commit()
+        self.db.commit()
 
     def insert_tarot_card(self, card):
         self.c.execute("INSERT INTO TarotCards (name, description, image) VALUES (?, ?, ?)", (card.name, card.description, card.image))
-        self.conn.commit()
+        self.db.commit()
 
-    def close_connection(self):
-        self.conn.close()
+    def add_user(self, chat_id, first_name, last_name, sign):
+        """Adds a user to the database"""
+        self.c.execute(
+        '''
+        INSERT OR IGNORE INTO subscriptions (chat_id, first_name, last_name, sign) 
+        VALUES (?, ?, ?, ?)
+        ''', (chat_id, first_name, last_name, sign))
+        self.db.commit()
     
+    def delete_person(self, chat_id):
+        """Deletes a user from the database"""
+        self.c.execute(
+        '''
+        DELETE FROM subscriptions WHERE chat_id = ?
+        ''', (chat_id,))
+        self.db.commit()
+    
+    def check_person(self, chat_id):
+        """Checks if a user exists"""
+        self.c.execute(
+        '''
+        SELECT * FROM subscriptions WHERE chat_id = ?
+        ''', (chat_id,))
+        return self.c.fetchone()
+
+    def fetch_all_users(self):
+        self.c.execute("SELECT * FROM Users")
+        rows = self.c.fetchall()
+        users = [User(row[1], row[2], row[3], row[4]) for row in rows]
+        return users
+
     def get_zodiac_signs(self):
         self.c.execute("SELECT * FROM ZodiacSigns")
         rows = self.c.fetchall()
@@ -39,15 +108,15 @@ class Model:
     def remove_duplicates_if_exists(self):
         self.c.execute("DELETE FROM ZodiacSigns WHERE rowid NOT IN (SELECT MIN(rowid) FROM ZodiacSigns GROUP BY name)")
         self.c.execute("DELETE FROM TarotCards WHERE rowid NOT IN (SELECT MIN(rowid) FROM TarotCards GROUP BY name)")
-        self.conn.commit()
+        self.db.commit()
 
     def clear_zodiac_signs(self):
         self.c.execute("DELETE FROM ZodiacSigns")
-        self.conn.commit()
+        self.db.commit()
     
     def clear_tarot_cards(self):
         self.c.execute("DELETE FROM TarotCards")
-        self.conn.commit()
+        self.db.commit()
 
 class ZodiacSign():
     def __init__(self, name, description, date, image):
